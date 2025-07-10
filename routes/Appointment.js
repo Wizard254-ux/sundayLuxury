8e230080a68b8d463ee82ce9f18963ff0ca9db31
Wilson-kali <wilsonndambuki47@gmail.com>
2025-07-10 07:41:53 +0300

    kk

--- routes/Appointment.js
+++ routes/Appointment.js
@@ -68,7 +68,6 @@ const sendApprovalEmail = async (appointment) => {
             <p style="color: #666; font-size: 14px;">
               Best regards,<br>
               <strong>${process.env.MAIL_SENDER_NAME || 'Project Manager Firm'}</strong><br>
-              📧 ${process.env.MAIL_USERNAME || 'cu.kibabiiuniversity@gmail.com'}
             </p>
           </div>
         </div>
